fd4bda7982751e3c39f9d09eeaff99fe72b57ad6
Mbaje Rwaka Eric <erickmbanjerwaka@gmail.com>
2022-05-20 15:51:17 +0300

    Create title_index.php

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/Logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Carlos Hilado Memorial State College - Talisay</p>
+							<h3>
+
+							<p>M - Learning</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>CHMSC EXCELS:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Science and Technology</p>
+								</div>		
+						</div>		
+				</div>
